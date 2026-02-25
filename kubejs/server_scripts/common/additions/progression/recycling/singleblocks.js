902a231251f50285760128f74cbd3575555f99ba
oTurquoise <turquoisesnake666@gmail.com>
2026-02-25 09:38:23 +0000

    added luv to uv singleblocks

--- kubejs/server_scripts/common/additions/progression/recycling/singleblocks.js
+++ kubejs/server_scripts/common/additions/progression/recycling/singleblocks.js
@@ -2,7 +2,7 @@
 global.not_hardmode(() => {
     ServerEvents.recipes(event => {
         const TIERS = [
-            "uhv", "uev", "uiv"
+            "luv", "zpm", "uv", "uhv", "uev", "uiv"
         ]
         const SINGLEBLOCKS = [
             "electric_furnace", "electric_blast_furnace", "electric_smoker", "alloy_smelter", "arc_furnace", "electrolyzer", "polarizer", 
@@ -14,94 +14,68 @@ global.not_hardmode(() => {
         const SINGLEBLOCKDETAILS = global.singleblockRecyclingDetails;
 
         function getSingleblockRecycleOutputs(arcBool, singleblock, specialSingleBool, tier, components /*not read if special*/, extraCasings /*0 if special*/, extraCables /*0 if special*/) {
-            const singleComponents = global.componentMaterials;
             const componentRecycles = global.componentRecycleMaterials;
-            const materials = {
-                casingMaterial: "",
-                primMaterial: "",
-                cableMaterial: "",
-                secMaterial: "",
-                tertMaterial: "",
-                wire: "",
-                elctrlyzWire: ""
-            }
+            const casingMaterials = global.casingMaterials;
+            let materialTypes = [];
+            let materials = {};
+            let blockType;
+            let getComponentTotal;
             const graphite = arcBool ? "7x gtceu:tiny_ash_dust" : "gtceu:graphite_dust";
             
-            switch(tier) {
-                case "uhv": {
-                    materials.casingMaterial = "gtceu:neutronium",
-                    materials.primMaterial = componentRecycles.uhv.primMaterial,
-                    materials.cableMaterial = componentRecycles.uhv.cableMaterial,
-                    materials.secMaterial = componentRecycles.uhv.secMaterial,
-                    materials.tertMaterial = componentRecycles.uhv.tertMaterial,
-                    materials.wire = `gtceu:${singleComponents.uhv.materials.wire}`,
-                    materials.elctrlyzWire = `gtceu:${singleComponents.uhv.materials.elctrlyzWire}`
-                    break;
-                }
-                case "uev": {
-                    materials.casingMaterial = "gtceu:mythrolic_alloy",
-                    materials.primMaterial = componentRecycles.uev.primMaterial,
-                    materials.cableMaterial = componentRecycles.uev.cableMaterial,
-                    materials.secMaterial = componentRecycles.uev.secMaterial,
-                    materials.tertMaterial = componentRecycles.uev.tertMaterial
-                    materials.wire = `gtceu:${singleComponents.uev.materials.wire}`,
-                    materials.elctrlyzWire = `gtceu:${singleComponents.uev.materials.elctrlyzWire}`
-                    break;
-                }
-                case "uiv": {
-                    materials.casingMaterial = "gtceu:chaotixic_alloy",
-                    materials.primMaterial = componentRecycles.uiv.primMaterial,
-                    materials.cableMaterial = componentRecycles.uiv.cableMaterial,
-                    materials.secMaterial = componentRecycles.uiv.secMaterial,
-                    materials.tertMaterial = componentRecycles.uiv.tertMaterial
-                    materials.wire = `gtceu:${singleComponents.uiv.materials.wire}`,
-                    materials.elctrlyzWire = `gtceu:${singleComponents.uiv.materials.elctrlyzWire}`
-                    break;
-                }
+            if (tier == "luv" || tier == "zpm" || tier == "uv") {
+                materialTypes = ["casing", "prim", "cable", "wire", "elctrlyzWire", "foil"];
+                getComponentTotal = global.getLUVToUVComponentTotal;
+                blockType = "singleblock_LUVToUV";
+            }
+            else if (tier == "uhv" || tier == "uev" || tier == "uiv") {
+                materialTypes = ["casing", "prim", "cable", "wire", "elctrlyzWire", "sec", "tert"];
+                getComponentTotal = global.getUHVPlusComponentTotal;
+                blockType = "singleblock_UHVPLUS"
             }
 
+            materialTypes.forEach(type => {
+                if (type == "casing") {
+                    materials[type + "Material"] = casingMaterials[tier];
+                }
+                else {
+                    materials[type + "Material"] = componentRecycles[tier][type + "Material"];
+                }
+            });
+
             const specialSingleOutputs = {
-                electric_furnace: [`8x ${materials.casingMaterial}`, `4x ${materials.wire}`, `2x ${materials.cableMaterial}`, " ", " ", " "],
-                electric_blast_furnace: [`10x ${materials.casingMaterial}`, `4x ${materials.wire}`, `2x ${materials.cableMaterial}`, " ", " ", " "],
-                electric_smoker: [`8x ${materials.casingMaterial}`, `6x ${materials.wire}`, `2x ${materials.cableMaterial}`, " ", " ", " "],
-                alloy_smelter: [`8x ${materials.casingMaterial}`, `6x ${materials.wire}`, `2x ${materials.cableMaterial}`, " ", " ", " "],
+                electric_furnace: [`8x ${materials.casingMaterial}`, `4x ${materials.wireMaterial}`, `2x ${materials.cableMaterial}`, " ", " ", " "],
+                electric_blast_furnace: [`10x ${materials.casingMaterial}`, `4x ${materials.wireMaterial}`, `2x ${materials.cableMaterial}`, " ", " ", " "],
+                electric_smoker: [`8x ${materials.casingMaterial}`, `6x ${materials.wireMaterial}`, `2x ${materials.cableMaterial}`, " ", " ", " "],
+                alloy_smelter: [`8x ${materials.casingMaterial}`, `6x ${materials.wireMaterial}`, `2x ${materials.cableMaterial}`, " ", " ", " "],
                 arc_furnace: [`11x ${materials.casingMaterial}`, `5x ${materials.cableMaterial}`, `${graphite}`, " ", " ", " "],
-                electrolyzer: [`8x ${materials.casingMaterial}`, `2x ${materials.elctrlyzWire}`, `1x ${materials.cableMaterial}`, " ", " ", " "],
+                electrolyzer: [`8x ${materials.casingMaterial}`, `2x ${materials.elctrlyzWireMaterial}`, `1x ${materials.cableMaterial}`, " ", " ", " "],
                 polarizer: [`18x ${materials.cableMaterial}`, `8x ${materials.casingMaterial}`, " ", " ", " ", " "],
-                charger_4x: [`8x ${materials.casingMaterial}`, `8x ${materials.wire}`, `2x ${materials.cableMaterial}`, " ", " ", " "]
+                charger_4x: [`8x ${materials.casingMaterial}`, `8x ${materials.wireMaterial}`, `2x ${materials.cableMaterial}`, " ", " ", " "]
             }
             let casingCount = 8 + extraCasings;
-            let recycleOutputs = [" ", " ", " ", " ", " ", " "];
+            let recycleOutputs = [];
             let tempTotals;
             let tempObj;
 
             if (specialSingleBool) {
-                if (singleblock == "electric_furnace") { recycleOutputs = specialSingleOutputs.electric_furnace; }
-                else if (singleblock == "electric_blast_furnace") { recycleOutputs = specialSingleOutputs.electric_blast_furnace; }
-                else if (singleblock == "electric_smoker") { recycleOutputs = specialSingleOutputs.electric_smoker; }
-                else if (singleblock == "alloy_smelter") { recycleOutputs = specialSingleOutputs.alloy_smelter; }
-                else if (singleblock == "arc_furnace") { recycleOutputs = specialSingleOutputs.arc_furnace; }
-                else if (singleblock == "electrolyzer") { recycleOutputs = specialSingleOutputs.electrolyzer; }
-                else if (singleblock == "polarizer") { recycleOutputs = specialSingleOutputs.polarizer; }
-                else if (singleblock == "charger_4x") { recycleOutputs = specialSingleOutputs.charger_4x; }
+                recycleOutputs = specialSingleOutputs[singleblock];
             }
             else {
                 // gets and checks the final outputs
-                tempTotals = global.getUHVPlusComponentTotal(components);
+                tempTotals = getComponentTotal(components);
                 tempTotals.cableCount += extraCables;
 
                 if (tier == "uev" || tier == "uiv") { // if tertiary material is the same as casing material
                     tempTotals.tertCount += casingCount;
-                    tempObj = global.checkRecyclingCount(tempTotals, "singleblock_UHVPLUS", false, false, false);
+                    tempObj = global.checkRecyclingCount(tempTotals, blockType, false, false, false);
                 }
-                else if (tier == "uhv") {
+                else {
                     tempTotals.casingCount = casingCount; 
-                    tempObj = global.checkRecyclingCount(tempTotals, "singleblock_UHVPLUS", false, true, false);
+                    tempObj = global.checkRecyclingCount(tempTotals, blockType, false, true, false);
                 }
                 
                 // sorts the final outputs
                 let checkCount = 0;
-                let position = 0;
                 let flag = false;
                 let flag2;
 
@@ -110,25 +84,19 @@ global.not_hardmode(() => {
                     if (checkCount == flag2) {
                         flag = true;
                     }
-                    if (tempObj.totals[tempObj.outputOrder[position] + "Count"] != 0) {
-                        recycleOutputs[position] = `${tempObj.totals[tempObj.outputOrder[position] + "Count"]}x ${materials[tempObj.outputOrder[position] + "Material"]}`;
-                        position++;
+                    if (tempObj.totals[tempObj.outputOrder[checkCount] + "Count"] != 0) {
+                        recycleOutputs.push(`${tempObj.totals[tempObj.outputOrder[checkCount] + "Count"]}x ${materials[tempObj.outputOrder[checkCount] + "Material"]}`);
                     }
                     
                     checkCount++;
                 }
-
-                recycleOutputs[position] = tempObj.blockBools.primBlock; position++;
-                recycleOutputs[position] = tempObj.blockBools.cableBlock; position++;
-                recycleOutputs[position] = (tier == "uhv" || tier == "uev" || tier == "uiv") ? tempObj.blockBools.secBlock : tempObj.blockBools.wireBlock; 
-                position++;
-                recycleOutputs[position] = (tier == "uhv" || tier == "uev" || tier == "uiv") ? tempObj.blockBools.tertBlock : tempObj.blockBools.foilBlock; 
-                position++;
+                // sets the blockBools
+                for (let n = 0; n < 4; n++) {
+                    recycleOutputs.push(tempObj.blockBools[tempObj.outputOrder[n] + "Block"]);
+                }
             }
 
-            if (recycleOutputs != undefined) {
-                return recycleOutputs;
-            }
+            return recycleOutputs;
         }
 
         
@@ -140,6 +108,10 @@ global.not_hardmode(() => {
             let outputs;
             
             tiers.forEach(tier => {
+                if (tier == "luv" || tier == "zpm" || tier == "uv") {
+                    event.remove({ input: `gtceu:${tier}_${singleblock}`, type: `gtceu:arc_furnace` });
+                }
+
                 outputs = getFinalOutputs(getSingleblockRecycleOutputs(true, singleblock, specialSingleBool, tier, components, extraCasings, extraCables), "singleblock", false, specialSingleBool);
                 event.recipes.gtceu.arc_furnace(id(`arc_${tier}_${singleblock}`))
                     .itemInputs(`gtceu:${tier}_${singleblock}`)
@@ -158,6 +130,10 @@ global.not_hardmode(() => {
             let outputs;
 
             tiers.forEach(tier => {
+                if (tier == "luv" || tier == "zpm" || tier == "uv") {
+                    event.remove({ input: `gtceu:${tier}_${singleblock}`, type: `gtceu:macerator` });
+                }
+
                 outputs = getFinalOutputs(getSingleblockRecycleOutputs(false, singleblock, specialSingleBool, tier, components, extraCasings, extraCables), "singleblock", true, specialSingleBool);
                 event.recipes.gtceu.macerator(id(`macerate_${tier}_${singleblock}`))
                     .itemInputs(`gtceu:${tier}_${singleblock}`)
