4190db7179fd41c469dd9f6dc3488e756d6594dc
Luzifer Senpai <luzifer@luzifersenpai.de>
2026-03-25 18:16:02 +0100

    CPA is now fully ZPM

--- kubejs/server_scripts/common/additions/multiblock_recipes/pre_gate_multis.js
+++ kubejs/server_scripts/common/additions/multiblock_recipes/pre_gate_multis.js
@@ -2,17 +2,17 @@ ServerEvents.recipes(event => {
     const id = global.id;
 
     event.recipes.gtceu.assembly_line(id('component_part_assembly'))
-        .itemInputs('gtceu:uv_assembler','8x gtceu:uv_robot_arm','8x gtceu:uv_conveyor_module',
-            '8x gtceu:uv_electric_pump', '4x #gtceu:circuits/uhv', '6x #gtceu:circuits/uv', '8x #gtceu:circuits/zpm')
+        .itemInputs('gtceu:zpm_assembler','8x gtceu:zpm_robot_arm','8x gtceu:zpm_conveyor_module',
+            '8x gtceu:zpm_electric_pump', '4x #gtceu:circuits/uv', '6x #gtceu:circuits/zpm', '8x #gtceu:circuits/luv')
         .inputFluids('gtceu:soldering_alloy 12528', 'gtceu:lubricant 2500')
         .itemOutputs('gtceu:component_part_assembly')
         .duration(1800)
         .stationResearch(
             researchRecipeBuilder => researchRecipeBuilder
-                .researchStack(Item.of('gtceu:uv_assembler'))
-                .EUt(GTValues.VHA[GTValues.UV])
-                .CWUt(96)
+                .researchStack(Item.of('gtceu:zpm_assembler'))
+                .EUt(GTValues.VHA[GTValues.ZPM])
+                .CWUt(16)
             )
-        .EUt(GTValues.VHA[GTValues.UV]);
+        .EUt(GTValues.VHA[GTValues.ZPM]);
     
 });
