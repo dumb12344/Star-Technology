f755ab4ab7588484d38653fe90cf3dc0ee7b9160
CosmicEcl07 <joeyachristopher@gmail.com>
2026-04-14 16:02:49 -0500

    -moved controller recipe

--- kubejs/server_scripts/common/additions/multiblock_recipes/abydos_multis.js
+++ kubejs/server_scripts/common/additions/multiblock_recipes/abydos_multis.js
@@ -120,5 +120,21 @@ ServerEvents.recipes(event => {
             )
         .duration(3200)
         .EUt(GTValues.VHA[GTValues.UV]);
-    
+
+        ServerEvents.recipes(event => {
+
+    event.recipes.gtceu.assembly_line(id('exotic_rock_crusher'))
+        .itemInputs('gtceu:large_material_press', '8x gtceu:uv_electric_piston', '4x gtceu:heat_vent', '4x gtceu:silicon_bronze_frame', '4x #gtceu:circuits/uv',
+         '2x gtceu:titan_steel_ultradense_plate', '8x gtceu:titanium_carbide_plate', '4x gtceu:hsla_steel_plate' )
+        .inputFluids('gtceu:titan_steel 1296', 'gtceu:soldering_alloy 3744')
+        .itemOutputs('gtceu:exotic_tectonic_formation_apparatus')
+        .duration(2400)
+        .stationResearch(
+            researchRecipeBuilder => researchRecipeBuilder
+                .researchStack(Item.of('gtceu:uv_rock_crusher'))
+                .EUt(GTValues.VHA[GTValues.ZPM])
+                .CWUt(24)
+            )
+        .EUt(GTValues.VHA[GTValues.UV]);
+        });
 });
