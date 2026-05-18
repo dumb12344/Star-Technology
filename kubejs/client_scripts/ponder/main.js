bcb476cc190fae7b8af14420a50b6050cfbb9fd5
n1xx1 <me@simonemiraglia.it>
2026-05-18 10:46:37 +0200

    ponder: fix gregtech machines list not being present at startup

--- kubejs/client_scripts/ponder/main.js
+++ kubejs/client_scripts/ponder/main.js
@@ -1,12 +1,86 @@
 // priority: -100
 
 (() => {
+  let getInitializationData = lazyGetter(() => {
+    return {
+      gregtechMultiblocks: getGregtechMachines("multiblock"),
+      stargateTravel: [
+        "sgjourney:classic_stargate",
+        "sgjourney:classic_stargate_base_block",
+        "sgjourney:classic_stargate_chevron_block",
+        "sgjourney:classic_stargate_ring_block",
+        "sgjourney:crystal_interface",
+        "sgjourney:classic_dhd",
+      ],
+    };
+  });
+
+  Ponder.tags((event) => {
+    event.createTag(
+      "kubejs:gtceu",
+      "gtceu:electric_blast_furnace",
+      "GregTech Multiblocks",
+      "Informations on how to use GregTech Multiblocks",
+      getInitializationData().gregtechMultiblocks,
+    );
+
+    event.createTag(
+      "kubejs:stargate",
+      "sgjourney:classic_stargate",
+      "Stargate Travel",
+      "Informations on how to use the Stargates of this modpack",
+      getInitializationData().stargateTravel,
+    );
+  });
+
+  Ponder.registry((event) => {
+    event
+      .create(getInitializationData().stargateTravel)
+      .scene(
+        "classic_stargate",
+        "Classic Stargate",
+        ponderScenes["classic_stargate"],
+      );
+
+    event
+      .create(getInitializationData().gregtechMultiblocks)
+      .scene(
+        "multiblock_introduction",
+        "Multiblock Introduction",
+        ponderScenes["multiblock_introduction"],
+      )
+      .scene(
+        "multiblock_construction",
+        "Multiblock Construction",
+        ponderScenes["multiblock_construction"],
+      )
+      .scene(
+        "multiblock_wallsharing",
+        "Multiblock Wall Sharing",
+        ponderScenes["multiblock_wallsharing"],
+      );
+  });
+
+  /**
+   * @template T
+   * @param {() => T} init
+   * @returns {() => T}
+   */
+  function lazyGetter(init) {
+    let data = null;
+    return () => {
+      if (!data) data = init();
+      return data;
+    };
+  }
+
   /**
    * @param {"multiblock"} kind
+   * @returns {string[]}
    */
   function getGregtechMachines(kind) {
     let $MultiblockMachineDefinition = Java.loadClass(
-      "com.gregtechceu.gtceu.api.machine.MultiblockMachineDefinition"
+      "com.gregtechceu.gtceu.api.machine.MultiblockMachineDefinition",
     );
 
     let isInstance = (clz, obj) => clz.__javaObject__.isInstance(obj);
@@ -22,59 +96,4 @@
 
     return result;
   }
-
-  let gtMachines = getGregtechMachines("multiblock"); //Doesnt Work rn / need to fix @n1xx1
-  let stargateBlocks = [
-    "sgjourney:classic_stargate",
-    "sgjourney:classic_stargate_base_block",
-    "sgjourney:classic_stargate_chevron_block",
-    "sgjourney:classic_stargate_ring_block",
-    "sgjourney:crystal_interface",
-    "sgjourney:classic_dhd",
-  ];
-
-  Ponder.tags((event) => {
-    event.createTag(
-      "kubejs:gtceu",
-      "gtceu:electric_blast_furnace", //use?
-      "GregTech Multiblocks",
-      "Informations on how to use GregTech Multiblocks",
-      gtMachines
-    );
-
-    event.createTag(
-      "kubejs:stargate",
-      "Stargate Travel",
-      "Informations on how to use the Stargates of this modpack",
-      stargateBlocks
-    );
-  });
-
-  Ponder.registry((event) => {
-    event
-      .create(stargateBlocks)
-      .scene(
-        "classic_stargate",
-        "Classic Stargate",
-        ponderScenes["classic_stargate"]
-      );
-
-    event
-      .create(gtMachines)
-      .scene(
-        "multiblock_introduction",
-        "Multiblock Introduction",
-        ponderScenes["multiblock_introduction"]
-      )
-      .scene(
-        "multiblock_construction",
-        "Multiblock Construction",
-        ponderScenes["multiblock_construction"]
-      )
-      .scene(
-        "multiblock_wallsharing",
-        "Multiblock Wall Sharing",
-        ponderScenes["multiblock_wallsharing"]
-      );
-  });
 })();
